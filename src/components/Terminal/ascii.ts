8fc5bee22f28550f0eae0c3551e31818136eb4fd
sagar budhathoki <sagar@eventlogic.se>
2025-04-28 10:23:59 +0545

    Coffee replaced with Lemon Tea

--- src/components/Terminal/ascii.ts
+++ src/components/Terminal/ascii.ts
@@ -64,7 +64,7 @@ export const asciiArt: Record<string, string> = {
                    │  └──┘    └──┘    │
                    └──────────────────┘`,
 
-  coffee: 
+  Lemon Tea: 
 `                       (
                         )     (
                  ___...(-------)-....___
